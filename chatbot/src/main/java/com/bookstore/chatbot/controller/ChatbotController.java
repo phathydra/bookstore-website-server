b613177ae6a7bb4b6fa01d89ace9ba50cfb74a7a
phathydra <toanphat124@gmail.com>
2025-04-13 03:10:05 +0700

    add basic flow control for chatbot

---
+++ chatbot/src/main/java/com/bookstore/chatbot/controller/ChatbotController.java
@@ -0,0 +1,11 @@
+package com.bookstore.chatbot.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/webhook")
+public class ChatbotController {
+}
